9a4c705672419180513a0259c88c92eee38d980e
akaaua <akaaua@gmail.com>
2022-08-14 03:55:39 -0400

    First commitgs

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react', 'prettier', 'react-hooks',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
---
+++ src/components/Main.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import './Main.css';
+
+//Form
+import { FaPlus } from 'react-icons/fa';
+
+//Tarefas
+import { FaWindowClose, FaEdit } from 'react-icons/fa';
+
+export default class Main extends Component {
+
+  state = {
+    novaTarefa: '',
+    tarefas: [
+      'Fazer café',
+      'Beber água',
+      'Estudar',
+    ]
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  }
+
+  render() {
+    const { novaTarefa, tarefas } = this.state;
+    return (
+      <div className='main'>
+        <h1>Lista de Tarefas</h1>
+
+        <form action="#" className='form'>
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}
+          />
+          <button type="submit">
+            <FaPlus />
+
+          </button>
+        </form>
+        <ul className='tarefas'>
+          {
+            tarefas.map(tarefa => (
+              <li key={tarefa}>
+                {tarefa}
+                <div>
+                  <FaEdit className='edit'/>
+                  <FaWindowClose className='delete'/>
+                </div>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    )
+  }
+}
